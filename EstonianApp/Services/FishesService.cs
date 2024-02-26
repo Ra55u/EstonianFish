5070fce7aeb9cad22b54e0cce42b485f7c40bf8e
Opilane TTHK <opilane@tthk.ee>
2024-02-26 12:15:01 +0200

    Commit

---
+++ EstonianApp/Services/FishesService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using EstonianApp.Models;
+
+namespace Services
+{
+    internal static class FishesService
+    {
+        private static List<Fish> fishes = new()
+        {
+            new()
+            {
+                Name = " Kalastus",
+                Description = "Mercury is the smallest planet in the Solar " +
+                "System and the closest to the Sun. Its orbit around the Sun takes " +
+                "87.97 Earth days, the shortest of all the Sun's planets. " +
+                "It is named after the Roman god Mercurius (Mercury), god " +
+                "of commerce, messenger of the gods, and mediator between gods " +
+                "and mortals, corresponding to the Greek god Hermes (Ἑρμῆς). Like " +
+                "Venus, Mercury orbits the Sun within Earth's orbit as an inferior " +
+                "planet; its apparent distance from the Sun as viewed from Earth " +
+                "never exceeds 28°."
+            },
+        };
+    }
+}
--- EstonianApp/Views/FishingPage.xaml.cs
+++ EstonianApp/Views/FishingPage.xaml.cs
@@ -1,13 +1,23 @@
+using Services;
+using EstonianApp.Models;
+using System.Numerics;
+
 namespace Views;
 
 public partial class FishingPage : ContentPage
 {
-	public FishingPage()
-	{
-		InitializeComponent();
-	}
-    async void BackButton_Clicked(object sender, EventArgs e)
+    public FishingPage(Fish fish)
     {
-        await Navigation.PopAsync();
+        InitializeComponent();
+
+        this.BindingContext = fish;
+    }
+
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+        async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
     }
 }
