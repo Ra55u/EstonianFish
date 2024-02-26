27f09a3ee447b2ee91ae63d007b6d65d65eb72b2
Opilane TTHK <opilane@tthk.ee>
2024-02-26 12:26:14 +0200

    FishingPage

--- EstonianApp/Views/FishingPage.xaml.cs
+++ EstonianApp/Views/FishingPage.xaml.cs
@@ -6,18 +6,12 @@ namespace Views;
 
 public partial class FishingPage : ContentPage
 {
-    public FishingPage(Fish fish)
+    public FishingPage()
     {
         InitializeComponent();
-
-        this.BindingContext = fish;
     }
-
-    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    async void BackButton_Clicked(object sender, EventArgs e)
     {
-        async void BackButton_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PopAsync();
-        }
+        await Navigation.PopAsync();
     }
 }
