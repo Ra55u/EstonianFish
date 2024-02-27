9328146d82adf52fe3cecba9866124d957f3140f
Opilane TTHK <opilane@tthk.ee>
2024-02-27 12:02:46 +0200

    mm

--- EstonianApp/Services/FishesService.cs
+++ EstonianApp/Services/FishesService.cs
@@ -13,15 +13,10 @@ namespace Services
             new()
             {
                 Name = " Kalastus",
-                Description = "Mercury is the smallest planet in the Solar " +
-                "System and the closest to the Sun. Its orbit around the Sun takes " +
-                "87.97 Earth days, the shortest of all the Sun's planets. " +
-                "It is named after the Roman god Mercurius (Mercury), god " +
-                "of commerce, messenger of the gods, and mediator between gods " +
-                "and mortals, corresponding to the Greek god Hermes (Ἑρμῆς). Like " +
-                "Venus, Mercury orbits the Sun within Earth's orbit as an inferior " +
-                "planet; its apparent distance from the Sun as viewed from Earth " +
-                "never exceeds 28°."
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultrices risus vel est congue, ac facilisis arcu tincidunt." +
+                " Suspendisse a nibh auctor, imperdiet metus vitae, mollis dolor. Curabitur tellus lectus, aliquam in ornare sed, pretium in augue. " +
+                "Phasellus feugiat fringilla consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos." +
+                " Curabitur finibus ut risus quis imperdiet. Curabitur dictum pellentesque mauris vitae consectetur. "
             },
         };
     }
