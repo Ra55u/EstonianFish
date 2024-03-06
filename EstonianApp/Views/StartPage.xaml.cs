8ea9a58fa57ee60d3438d7dd34a157b9ac9a915e
arturmuursepp <artur.ateam@gmail.com>
2024-03-06 13:24:42 +0200

    MAIN COMMIT LOL!

--- EstonianApp/Views/StartPage.xaml.cs
+++ EstonianApp/Views/StartPage.xaml.cs
@@ -8,4 +8,11 @@ public partial class StartPage : ContentPage
 	}
     async void kalastus_Clicked(System.Object sender, System.EventArgs e)
             => Application.Current.MainPage = new NavigationPage(new FishingPage());
+    async void fish_Clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new FishSpeciesPage());
+    async void water_clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new BodyOfWater());
+    async void home_clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new StartPage());
+
 }
