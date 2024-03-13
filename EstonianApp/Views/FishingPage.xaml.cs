ba82a9b81c041e1ffa854614d2be5557aefdc889
Opilane TTHK <opilane@tthk.ee>
2024-03-13 14:55:52 +0200

    WorkinggfishingPAGEE"!!

--- EstonianApp/Views/FishingPage.xaml.cs
+++ EstonianApp/Views/FishingPage.xaml.cs
@@ -1,4 +1,3 @@
-using Services;
 using EstonianApp.Models;
 using System.Numerics;
 
@@ -10,8 +9,12 @@ public partial class FishingPage : ContentPage
     {
         InitializeComponent();
     }
-    //async void water_clicked(System.Object sender, System.EventArgs e)
-    //    => Application.Current.MainPage = new NavigationPage(new BodyOfWater());
+    async void fishing_Clicked(System.Object sender, System.EventArgs e)
+            => Application.Current.MainPage = new NavigationPage(new FishingPage());
+    async void fish_Clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new FishingPage());
+    async void water_clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new StartPage());
     async void home_clicked(System.Object sender, System.EventArgs e)
         => Application.Current.MainPage = new NavigationPage(new StartPage());
 }
--- EstonianApp/Views/StartPage.xaml.cs
+++ EstonianApp/Views/StartPage.xaml.cs
@@ -4,16 +4,16 @@ namespace Views;
 
 public partial class StartPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
-    //async void kalastus_Clicked(System.Object sender, System.EventArgs e)
-    //        => Application.Current.MainPage = new NavigationPage(new FishingPage());
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+    async void fishing_Clicked(System.Object sender, System.EventArgs e)
+           => Application.Current.MainPage = new NavigationPage(new FishingPage());
     async void fish_Clicked(System.Object sender, System.EventArgs e)
         => Application.Current.MainPage = new NavigationPage(new FishSpeciesPage());
-    //async void water_clicked(System.Object sender, System.EventArgs e)
-    //    => Application.Current.MainPage = new NavigationPage(new BodyOfWater());
+    async void water_clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new StartPage());
     async void home_clicked(System.Object sender, System.EventArgs e)
         => Application.Current.MainPage = new NavigationPage(new StartPage());
 
