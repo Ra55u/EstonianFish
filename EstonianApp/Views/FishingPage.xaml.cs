9bd02beddf0d4538b9263a6c927cd123d2daa794
Opilane TTHK <opilane@tthk.ee>
2024-03-13 14:43:11 +0200

    added photos

--- EstonianApp/Views/FishingPage.xaml.cs
+++ EstonianApp/Views/FishingPage.xaml.cs
@@ -1,20 +1,20 @@
-using EstonianApp.Views;
+using EstonianApp.Models;
+using System.Numerics;
 
 namespace Views;
 
-public partial class StartPage : ContentPage
+public partial class FishingPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
+    public FishingPage()
+    {
+        InitializeComponent();
+    }
     async void fishing_Clicked(System.Object sender, System.EventArgs e)
-           => Application.Current.MainPage = new NavigationPage(new FishingPage());
+            => Application.Current.MainPage = new NavigationPage(new FishingPage());
     async void fish_Clicked(System.Object sender, System.EventArgs e)
-        => Application.Current.MainPage = new NavigationPage(new FishSpeciesPage());
+        => Application.Current.MainPage = new NavigationPage(new FishingPage());
     async void water_clicked(System.Object sender, System.EventArgs e)
         => Application.Current.MainPage = new NavigationPage(new StartPage());
     async void home_clicked(System.Object sender, System.EventArgs e)
         => Application.Current.MainPage = new NavigationPage(new StartPage());
-
 }
