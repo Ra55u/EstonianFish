0d056914ed10e2778647eebd5a0f13cd5619d030
arturmuursepp <artur.ateam@gmail.com>
2024-03-20 13:52:17 +0200

    s

--- EstonianApp/Views/StartPage.xaml.cs
+++ EstonianApp/Views/StartPage.xaml.cs
@@ -1,3 +1,4 @@
+using EstonianApp.Models;
 using EstonianApp.Views;
 
 namespace Views;
@@ -7,6 +8,12 @@ public partial class StartPage : ContentPage
     public StartPage()
     {
         InitializeComponent();
+
+        lstPopularFishSpecies.ItemsSource = EstonianApp.Services.FishService.GetFeaturedFish();
+    }
+    async void Fish_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        await Navigation.PushAsync(new FishDetailsPage(e.CurrentSelection.First() as Fish));
     }
     async void kalastus_Clicked(System.Object sender, System.EventArgs e)
             => Application.Current.MainPage = new NavigationPage(new FishSpeciesPage());
